e4696a04cbc003737d7ba28d58b14775e9fc2682
Dougal Matthews <dougal@dougalmatthews.com>
2014-04-30 22:13:54 +0100

    Add an initial ghetto asyncio test.

--- tests/transport/test_asyncio.py
+++ tests/transport/test_asyncio.py
@@ -1,5 +1,24 @@
-from unittest import TestCase
+from asyncio import get_event_loop
+from unittest import TestCase, mock
+
+from rfxcom.transport import AsyncioTransport
+
+from rfxcom.protocol import RESET_PACKET, STATUS_PACKET
 
 
 class AsyncioTransportTestCase(TestCase):
-    pass
+
+    def test_loop_once(self):
+
+        loop = get_event_loop()
+
+        def handler(*args, **kwargs):
+            pass
+
+        device = mock.MagicMock()
+
+        AsyncioTransport(device, loop, callback=handler)
+        loop._run_once()
+
+        device.write.assert_has_call(bytearray(RESET_PACKET))
+        device.write.assert_has_call(bytearray(STATUS_PACKET))
