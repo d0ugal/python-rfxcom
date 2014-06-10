6b16117b0241e63f148d4b47641622dfa045d409
Cosimo Alfarano <cosimo@alfarano.net>
2014-06-10 16:48:12 +0100

    unit test for AsyncioTransport rewritten for constructor and setup
    The idea is removing the dependancies, which in this case are the loop
    and the device objects, and testing only their role with
    AsyncioTransport

--- tests/transport/test_asyncio.py
+++ tests/transport/test_asyncio.py
@@ -1,24 +1,28 @@
-from asyncio import get_event_loop
 from unittest import TestCase, mock
 
 from rfxcom.transport import AsyncioTransport
-
-from rfxcom.protocol import RESET_PACKET, STATUS_PACKET
+from rfxcom.protocol import RESET_PACKET, MODE_PACKET, STATUS_PACKET
 
 
 class AsyncioTransportTestCase(TestCase):
 
-    def test_loop_once(self):
+    @mock.patch('asyncio.AbstractEventLoop')
+    @mock.patch('serial.Serial')
+    def test_transport_constructor(self, device, loop):
+        unit = AsyncioTransport(device, loop, callback=mock.Mock())
+        loop.add_writer.assert_called_once_with(device.fd, unit.setup)
 
-        loop = get_event_loop()
+    @mock.patch('asyncio.AbstractEventLoop')
+    @mock.patch('serial.Serial')
+    @mock.patch('rfxcom.transport.AsyncioTransport.write')
+    def test_transport_setup(self, unit_write, device, loop):
 
-        def handler(*args, **kwargs):
-            pass
+        unit = AsyncioTransport(device, loop, callback=mock.Mock())
+        unit.setup()
 
-        device = mock.MagicMock()
-
-        AsyncioTransport(device, loop, callback=handler)
-        loop._run_once()
-
-        device.write.assert_has_call(bytearray(RESET_PACKET))
-        device.write.assert_has_call(bytearray(STATUS_PACKET))
+        loop.remove_writer.assert_called_with(device.fd)
+        device.write.assert_called_once(RESET_PACKET)
+        loop.call_later.assert_any_call(mock.ANY,
+                                        super(AsyncioTransport, unit).write,
+                                        STATUS_PACKET)
+        unit_write.assert_called_once_with(MODE_PACKET)
